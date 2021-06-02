9fa12c7a2f3e45c00679168b2beece4355578e4d
B0544218 <32070909+B0544218@users.noreply.github.com>
2021-06-02 11:24:37 +0800

    Update load_csv_mysql command line.sql

--- load_csv_mysql command line.sql
+++ load_csv_mysql command line.sql
@@ -1,10 +1,11 @@
-#載入csv進MySQL坑
+##  載入csv進MySQL坑
 #  Error Code: 1290.  step1:  windows下：修改my.ini (預設，C:\ProgramData\MySQL\MySQL Server 8.0) 的secure_file_priv = ''
 #  並重啟 mysql服務
 #  Error Code: 3948.  step2: SHOW GLOBAL VARIABLES LIKE 'local_infile'; 如果為off則改為ON  SET GLOBAL local_infile=1;
-#  在command line輸入 mysql --local-infile=1 -u root -p
-#  step3: use 某一個database，然後輸入下面load指令
+## 切換到command line (在workbench或mysql commandline 會有bug) 
+#  在cmd輸入 mysql --local-infile=1 -u root -p
 
+use 某一database;
 LOAD DATA local INFILE 'C:\\worksp\\mysql\\discounts.csv' 
 INTO TABLE discounts 
 FIELDS TERMINATED BY ','
